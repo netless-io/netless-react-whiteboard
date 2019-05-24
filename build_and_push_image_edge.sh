93b727c9a767c7e05ce66d54f3d685ad8ac6f017
buhe <hare.bu@herewhite.com>
2019-05-24 11:18:10 +0800

    add ci scriprt and nginx.conf

---
+++ build_and_push_image_edge.sh
@@ -0,0 +1,14 @@
+yarn
+yarn build
+image=netless-react-whiteboard
+version=1.0.0
+
+hash=$(git rev-parse --short HEAD)
+
+docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash -t registry.cn-hangzhou.aliyuncs.com/white/$image:latest .
+docker push registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash
+docker push registry.cn-hangzhou.aliyuncs.com/white/$image:latest
+
+ssh app@k8s-cloud -tt "cd /home/app/k8s-stack/demo/netless-react-whiteborad && \
+    kubectl apply -f deploy.yml && \
+    kubectl patch deployment netless-react-whiteborad -n demo --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
