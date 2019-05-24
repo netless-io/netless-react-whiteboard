5dd4aa2477d5cc8ba217f783655217f10e139353
buhe <hare.bu@herewhite.com>
2019-05-24 18:17:00 +0800

    speed up ts compile

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ const basic = {
         filename: "javascript/index-[hash].js",
         path: __dirname + "/build",
         publicPath: "/",
+        pathinfo: false
     },
 
     resolve: {
@@ -36,7 +37,12 @@ const basic = {
             }, {
                 test: /\.tsx?$/,
                 use: [
-                    'ts-loader',
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                        },
+                    },
                     {
                         loader: 'ui-component-loader',
                         options: {
@@ -81,7 +87,12 @@ const basic = {
             path: __dirname + "/build",
             inject: "body",
         }),
-    ]
+    ],
+    optimization: {
+        removeAvailableModules: false,
+        removeEmptyChunks: false,
+        splitChunks: false,
+    }
 };
 
 const development = {
