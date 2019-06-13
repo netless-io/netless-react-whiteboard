373f918e6b66d48d94a62cff0e709fdd3aaaa394
Moskize91 <i@taozeyu.com>
2019-06-13 18:19:16 +0800

    refactor: NameInputPage

--- src/pages/NameInputPage.tsx
+++ src/pages/NameInputPage.tsx
@@ -1,31 +1,39 @@
 import * as React from "react";
-import * as uuidv4 from "uuid/v4";
-import {Input, Button} from "antd";
+import * as uuid from "uuid/v4";
+
 import "./PageNameInput.less";
+
+import NetlessBlack from "../assets/image/netless_black.svg";
+
+import {Input, Button} from "antd";
 import {withRouter} from "react-router-dom";
 import {RouteComponentProps} from "react-router";
-import netless_black from "../assets/image/netless_black.svg";
 import {Link} from "@netless/i18n-react-router";
 import {netlessWhiteboardApi} from "../apiMiddleware";
 
-
 export type PageNameInputProps = RouteComponentProps<{}>;
-export type PageNameInputStates = {
-    name: string;
+export type PageNameInputState = {
+    readonly name: string;
 };
 
-class NameInputPage extends React.Component<PageNameInputProps, PageNameInputStates> {
+class NameInputPage extends React.Component<PageNameInputProps, PageNameInputState> {
+
     public constructor(props: PageNameInputProps) {
         super(props);
         this.state = {
             name: "",
         };
     }
-    private handleClickBtn = (): void => {
+
+    private onInputNameChanged(name: string): void {
+        this.setState({name: name.trim()});
+    }
+
+    private onClickButton = (): void => {
         if (this.state.name) {
-            netlessWhiteboardApi.user.updateUserInf(this.state.name, uuidv4(), "1");
+            netlessWhiteboardApi.user.updateUserInf(this.state.name, uuid(), "1");
         } else {
-            netlessWhiteboardApi.user.updateUserInf("Netless user", uuidv4(), "1");
+            netlessWhiteboardApi.user.updateUserInf("Netless user", uuid(), "1");
         }
         this.props.history.push("/whiteboard/");
     }
@@ -34,23 +42,26 @@ class NameInputPage extends React.Component<PageNameInputProps, PageNameInputSta
         return (
             <div className="page-input-box">
                 <Link to="/">
-                    <img src={netless_black}/>
+                    <img src={NetlessBlack}/>
                 </Link>
                 <div className="page-input-left-box">
                     <div className="page-input-left-mid-box">
                         <div className="name-title">输入临时用户名字<br/>可以方便您在互动的时候区别身份</div>
-                        <Input onChange={e => this.setState({name: e.target.value})} size={"large"} placeholder={"输入用户名"}/>
-                        <Button
-                            size="large"
-                            type="primary"
-                            onClick={this.handleClickBtn}
-                            className="name-button">
+                        <Input size="large"
+                               placeholder="输入用户名"
+                               value={this.state.name}
+                               onChange={e => this.onInputNameChanged(e.target.value)}/>
+                        <Button size="large"
+                                type="primary"
+                                onClick={this.onClickButton}
+                                className="name-button">
                             确认
                         </Button>
                     </div>
                 </div>
                 <div className="page-input-right-box"/>
-            </div>);
+            </div>
+        );
     }
 }
 
