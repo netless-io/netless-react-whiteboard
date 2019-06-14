d97f7aca415000747f98fd2cb8610acf1a14c10e
Moskize91 <i@taozeyu.com>
2019-06-14 21:00:52 +0800

    refactor: WhiteboardPage

--- room/src/index.ts
+++ room/src/index.ts
@@ -1,2 +1,5 @@
+export {createOSS, OSSOptions, OSSBucketInformation} from "./tools/OSSCreator";
+export {UserPayload} from "./common/UserPayload";
+export {LoadingPage} from "./components/LoadingPage";
 export {RealtimeRoomPageProps, RealtimeRoomPageCallbacks, RealtimeRoomPageState} from "./realtime/RealtimeRoomPage";
 export {default as RealtimeRoomPage} from "./realtime/RealtimeRoomPage";
--- website/src/AppOptions.ts
+++ website/src/AppOptions.ts
@@ -1,14 +1,16 @@
+import {OSSOptions} from "netless-whiteboard-room";
+
 const config = require("./tokenConfig");
 
 export const netlessToken = {
     sdkToken: config.netlessToken,
 };
 
-export const ossConfigObj = {
+export const ossOptions: OSSOptions = Object.freeze({
     accessKeyId: config.ossConfigObj.accessKeyId,
     accessKeySecret: config.ossConfigObj.accessKeySecret,
     region: config.ossConfigObj.region,
     bucket: config.ossConfigObj.bucket,
     folder: config.ossConfigObj.folder,
     prefix: config.ossConfigObj.prefix,
-};
+});
