be6d85992a243dfa8e1c1dfb9fd49380b0d1d056
Moskize91 <i@taozeyu.com>
2019-06-14 20:18:39 +0800

    export as lib

---
+++ room/src/index.ts
@@ -0,0 +1,2 @@
+export {RealtimeRoomPageProps, RealtimeRoomPageCallbacks, RealtimeRoomPageState} from "./realtime/RealtimeRoomPage";
+export {default as RealtimeRoomPage} from "./realtime/RealtimeRoomPage";
