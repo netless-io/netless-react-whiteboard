c27d4ae54fa1bed476db61a43199f07883a4daf9
Moskize91 <i@taozeyu.com>
2019-06-15 15:44:17 +0800

    add libs

--- room/src/index.ts
+++ room/src/index.ts
@@ -2,4 +2,4 @@ export {createOSS, OSSOptions, OSSBucketInformation} from "./tools";
 export {UserPayload} from "./common";
 export {LoadingPage} from "./components";
 export {RealtimeRoomPageProps, RealtimeRoomPageCallbacks, RealtimeRoomPageState} from "./realtime";
-export {default as RealtimeRoomPage} from "./realtime";
+export {default as RealtimeRoomPage} from "./realtime/RealtimeRoomPage";
