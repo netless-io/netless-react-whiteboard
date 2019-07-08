cb5f4f13205e1c2b544ebc87bc4a804fcc273c1d
Moskize91 <i@taozeyu.com>
2019-07-08 11:59:34 +0800

    convert dynamic ppt preview to jpeg

--- room/scripts/svg2base64.js
+++ room/scripts/svg2base64.js
@@ -8,10 +8,7 @@ const distPath = path.resolve(workspace, "dist");
 
 searchFiles(sourcePath, "assets", function(filePath) {
     const absoluteFilePath = path.resolve(sourcePath, filePath);
-    const fileContent = fs.readFileSync(absoluteFilePath, "utf8");
-    const base64 = new Buffer(fileContent).toString("base64");
-
-    const targetContent = "exports.default = \"data:image/svg+xml;base64," + base64 + "\";";
+    const targetContent = convertToBase64(absoluteFilePath);
     const targetPath = path.resolve(distPath, filePath + ".js");
     const targetDirectory = targetPath.replace(/\/(\w|\.)+$/i, "");
 
@@ -19,6 +16,26 @@ searchFiles(sourcePath, "assets", function(filePath) {
 
     fs.writeFileSync(targetPath, targetContent, "utf8");
 });
+
+function convertToBase64(filePath) {
+    switch (filePath.match(/\.[a-zA-Z0-9]+$/i)[0]) {
+        case ".svg": {
+            const fileContent = fs.readFileSync(filePath, "utf8");
+            const base64 = new Buffer(fileContent).toString("base64");
+
+            return "exports.default = \"data:image/svg+xml;base64," + base64 + "\";";
+        }
+        case ".jpeg": {
+            const fileBuffer = fs.readFileSync(filePath);
+            const base64 = fileBuffer.toString("base64");
+
+            return "exports.default = \"data:image/jpeg;base64," + base64 + "\";";
+        }
+        default: {
+            throw new Error("unexpect file format " + filePath);
+        }
+    }
+}
 
 function searchFiles(basicPath, directory, fileHandler) {
     const files = fs.readdirSync(path.resolve(basicPath, directory));
